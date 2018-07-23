06bb83d9b46fac43558f63bbb58e267870311d43
Nicolas Letellier <mrletelliernicolas@yahoo.fr>
2018-07-23 16:51:18 +0200

    add improvements

--- index.js
+++ index.js
@@ -17,40 +17,49 @@ let options = ['2018-07-20', 10, 300, url2];
 let id = '01a48ec1e1c3668a7f55f401efc53388';
 
 function stats(jsonBody, options) {
-  console.log('______________\nDay ' + options[0]);
-  console.log('Arena: ' + options[3].replace('https://hola.org/challenges/haggling/scores/', '') + '\n');
 
-  let allDayParticipants = [];
   let allDayParticipantsSessions = [];
+  let mySessions;
+  let totalDaySessions = 0;
 
   for (let participant in jsonBody) {
     for (let day in jsonBody[participant]) {
       if (day === options[0] && jsonBody[participant][day]['sessions'] > options[2]) {
-        allDayParticipants.push(participant);
         allDayParticipantsSessions.push(jsonBody[participant][day]);
+        totalDaySessions += jsonBody[participant][day]['sessions'];
         if (participant === id) {
-          console.log('> My score: ' + (jsonBody[participant][day]['score'] / jsonBody[participant][day]['sessions']).toFixed(2) + '\n');
+          mySessions = jsonBody[participant][day];
         }
       }
     }
   }
 
-  let sessionAverageScores = [];
+  console.log('_____________________________________\nDay ' + options[0] + '  /  Arena: ' + options[3].replace('https://hola.org/challenges/haggling/scores/', '') + '\n');
+
+  if (mySessions) {
+    console.log('> MY SCORE:   ' + (mySessions.score / mySessions.sessions).toFixed(2) + '   Agreed: ' + (mySessions.agreements / mySessions.sessions).toFixed(2) + '\n');
+  }
+
   let totalSessionAverageScores = 0;
 
   for (let i = 0; i < allDayParticipantsSessions.length; i++) {
     totalSessionAverageScores += (allDayParticipantsSessions[i]['score'] / allDayParticipantsSessions[i]['sessions']);
-    sessionAverageScores.push(allDayParticipantsSessions[i]['score'] / allDayParticipantsSessions[i]['sessions']);
+    allDayParticipantsSessions[i]['scorePerSession'] = (allDayParticipantsSessions[i]['score'] / allDayParticipantsSessions[i]['sessions']);
+    allDayParticipantsSessions[i]['agreementsPercentage'] = (allDayParticipantsSessions[i]['agreements'] / allDayParticipantsSessions[i]['sessions']);
   }
 
-  console.log('Score per session Average: ' + (totalSessionAverageScores / allDayParticipantsSessions.length).toFixed(2) + '  (minimum sessions: ' + options[2] + ' / total ids: ' + sessionAverageScores.length + ')\n');
+  console.log('Score per session Average:  ' + (totalSessionAverageScores / allDayParticipantsSessions.length).toFixed(2) + '  (minimum sessions: ' + options[2] + ' / total ids: ' + allDayParticipantsSessions.length + ' / total sessions: ' + totalDaySessions + ')\n');
 
-  sessionAverageScores.sort(function (a, b) {
-    return b - a;
+  allDayParticipantsSessions.sort(function (a, b) {
+    return b.scorePerSession - a.scorePerSession;
   });
 
-  for (let i = 0; i < options[1]; i++) {
-    console.log('# ' + (i + 1) + ' : ' + sessionAverageScores[i].toFixed(2));
+  let maxTopParticipants = options[1];
+  if (allDayParticipantsSessions.length < options[1]) {
+    maxTopParticipants = allDayParticipantsSessions.length;
+  }
+  for (let i = 0; i < maxTopParticipants; i++) {
+    console.log('# ' + (i + 1) + '     ' + allDayParticipantsSessions[i]['scorePerSession'].toFixed(2) + '      Agreed: ' + allDayParticipantsSessions[i]['agreementsPercentage'].toFixed(2) + ' - Sessions: ' + allDayParticipantsSessions[i]['sessions']);
   }
 
 }
